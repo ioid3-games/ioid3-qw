a76d7bad7812b3290db1f2a8f34c567b89fe97e3
ToKu <toku@gmail.com>
2020-12-26 09:12:11 +0100

    ioquake3 -> 3566.

--- code/game/ai_dmq3.c
+++ code/game/ai_dmq3.c
@@ -3968,7 +3968,7 @@ const int BotFindEnemy(bot_state_t *bs, int curenemy) {
 		target[2] += 1;
 
 		BotAI_Trace(&trace, bs->eye, NULL, NULL, target, bs->client, CONTENTS_SOLID);
-
+		// if a full trace or the hitent is hit
 		if (trace.fraction >= 1 || trace.entityNum == goal->entitynum) {
 			if (goal->entitynum == bs->enemy) {
 				return qfalse;
